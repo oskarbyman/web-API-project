284413f3e3ff76387c8c4f066b00a9f6f80da2cf
oskarbyman <oskar.byman@gmail.com>
2022-03-10 19:35:34 +0200

    Add routing to all resources

--- workoutplanner/api.py
+++ workoutplanner/api.py
@@ -16,5 +16,24 @@ from workoutplanner.resources.workout_plan import WorkoutPlanCollection, Workout
 api_bp = Blueprint("api", __name__, url_prefix="/api")
 api = Api(api_bp)
 
-#TODO: Add routing for rest of the resources
+#  User resources from resources/user.py
 api.add_resource(UserCollection, "/users/")
+api.add_resource(UserItem, "/users/<user>")
+
+#  Move resources from resources/move.py
+api.add_resource(MoveCollection, "/users/<user>/moves")
+api.add_resource(MoveCollection, "/moves")
+api.add_resource(MoveItem, "/users/<user>/moves/<move>")
+api.add_resource(MoveItem, "/moves/<move>")
+
+#  Workout resources from resource/workout_plan.py
+api.add_resource(WorkoutPlanCollection, "/users/<user>/workouts")
+api.add_resource(WorkoutPlanCollection, "/workouts")
+api.add_resource(WorkoutPlanItem, "/users/<user>/workouts/<workout>")
+api.add_resource(WorkoutPlanItem, "/workouts/<workout>")
+
+#  MoveListItem resources from resources/move_list_item.py
+api.add_resource(MoveListItemCollection, "/users/<user>/workouts/<workout>/moves")
+api.add_resource(MoveListItemCollection, "/workouts/<workout>/moves")
+api.add_resource(MoveListItemItem, "/users/<user>/workouts/<workout>/moves/<int:position>")
+api.add_resource(MoveListItemItem, "/workouts/<workout>/moves/<int:position>")
